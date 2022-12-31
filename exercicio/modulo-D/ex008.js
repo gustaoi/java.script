d2578b54cd5cad0cde51476678e23d8e8dc6fb82
gustavo santos <110102661+gustaoi@users.noreply.github.com>
2022-12-31 17:28:24 -0300

    ex008 / 009 / 010 / 011 finalizado

---
+++ exercicio/modulo-D/ex008.js
@@ -0,0 +1,9 @@
+// --- Condição Simples ---
+
+let vel = 60
+
+console.log(`A velocidasde do seu carro é ${vel}km/h`)
+if (vel >=  60) { 
+    console.log(`Você passou do limete de velocidade / MULTADO`)
+}
+console.log(`Dirija sempre usando cinto`)
