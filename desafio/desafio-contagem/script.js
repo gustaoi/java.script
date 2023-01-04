9a48963d767027a6e0bf80f970fa8ed3240641f9
gustavo santos <110102661+gustaoi@users.noreply.github.com>
2023-01-04 16:32:24 -0300

    desafio-contagem

--- desafio/desafio-contagem/script.js
+++ desafio/desafio-contagem/script.js
@@ -1,18 +1,18 @@
 function contar() {
-    let ini = window.document.querySelector('input#iinicio')
-    let fim = window.document.querySelector('input#ifim')
-    let passo = window.document.querySelector('input#ipasso')
+    var ini = window.document.querySelector('input#iinicio')
+    var fim = window.document.querySelector('input#ifim')
+    var passo = window.document.querySelector('input#ipasso')
 
-    let res = document.getElementById('iresultado')
+    var res = window.document.getElementById('iresultado')
 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
         window.alert('[ERRO]')
     } else {
         res.innerHTML = 'Contando:'
 
-        let i = Number(ini.value)
-        let f = Number(fim.value)
-        let p = Number(passo.value)
+        var i = Number(ini.value)
+        var f = Number(fim.value)
+        var p = Number(passo.value)
 
         for (let c = i; c <= f; c += p) {
             res.innerHTML += `${c}`
