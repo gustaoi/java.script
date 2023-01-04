144255a2918f071f93b32963b87cd834a1948216
gustavo santos <110102661+gustaoi@users.noreply.github.com>
2023-01-04 20:18:26 -0300

    desafio-contador finalizado

--- desafio/desafio-contagem/script.js
+++ desafio/desafio-contagem/script.js
@@ -1,22 +1,35 @@
 function contar() {
-    var ini = window.document.querySelector('input#iinicio')
-    var fim = window.document.querySelector('input#ifim')
-    var passo = window.document.querySelector('input#ipasso')
-
-    var res = window.document.getElementById('iresultado')
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        window.alert('[ERRO]')
+        res.innerHTML = 'Impossivel Contar'
     } else {
-        res.innerHTML = 'Contando:'
+        res.innerHTML = 'Contando... <br>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
 
-        var i = Number(ini.value)
-        var f = Number(fim.value)
-        var p = Number(passo.value)
-
-        for (let c = i; c <= f; c += p) {
-            res.innerHTML += `${c}`
-            
+        if (p <= 0) {
+            window.alert('Passo inválido! Considerando PASSO 1')
+            p = 1
         }
+
+        if (i < f) {
+        // Contagem Crescente
+            for(let c = i; c <= f; c += p) {
+            res.innerHTML += `${c} \u{1F449}` // codigo de emoji
+        }
+
+        } else {
+        // Contagem Decrescente
+            for(let c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
+            res.innerHTML += `\u{1F3C1}`
+        }
+        
     }
 }
